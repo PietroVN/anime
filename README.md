c57de5b0ebef56a5dee1523f4058e08ad33d52c2
PietroVN <pietrovn7@protonmail.com>
2022-02-11 22:48:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Video
+Watch youtube videos without needing to use youtube
+I plan adding more support later
+
+# Deps
+* Search (Included)
+* Mpv
+* YoutubeDL
+* Sed
+* Curl
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: PietroVN
+# Install script for video
+
+# Check if we are running as root
+[[ ${UID} != "0" ]] && echo "install.sh need root permissions"
+[[ ${UID} != "0" ]] && exit 0
+
+# Soft link to /usr/local/bin
+ln -s $(pwd)/music /usr/local/bin/video
+ln -s $(pwd)/deps/search/search /usr/local/bin/search
+
+# Give executable permissions
+chmod +x /usr/local/bin/video
+chmod +x /usr/local/bin/search
