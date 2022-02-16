f8501515841bb0c5b9a00d22500833d6c6c60e08
PietroVN <pietrovn7@protonmail.com>
2022-02-16 15:27:36 -0300

    Improvements: fix install.sh anime search now only serves to add animes to your list you can fetch anime episodes list (this is recommended whenever a new episode is released)

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 [[ ${UID} != "0" ]] && exit 0
 
 # Soft link to /usr/local/bin
-ln -s $(pwd)/music /usr/local/bin/video
+ln -s $(pwd)/video /usr/local/bin/video
 ln -s $(pwd)/deps/search/search /usr/local/bin/search
 
 # Give executable permissions
